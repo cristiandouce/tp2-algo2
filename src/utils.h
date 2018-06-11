b00fc15ebe6eb1e2b326a02b32a5a740b7513628
ajd <augusto.dreiling@gmail.com>
2018-06-10 22:00:14 -0300

    Se agregan comentarios a funcion next_power2

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,6 @@
+#ifndef _UTILS_H_INCLUDED_
+#define _UTILS_H_INCLUDED_
+
 /**
  * @brief Retorna un numero mayor al entero provisto
  *        que sea potencia de 2
@@ -6,3 +9,5 @@
  * @return unsigned int
  */
 unsigned int next_power2 (unsigned int const &l);
+
+#endif
